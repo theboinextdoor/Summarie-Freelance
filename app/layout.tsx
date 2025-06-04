3a38656b97cf59d42566b4044ee774f3a5782632
theboynextdoor <cman55245@gmail.com>
2025-06-04 13:58:22 +0530

    made a chages

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,21 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-  title: "Summaire- AI Powered PDF Summarization",
-  description: "Generated Summary from PDF",
+  title: 'Summaire- AI Powered PDF Summarization',
+  description:
+    'Save hours of reading time, Transform lengthy PDFs into clear, accurate summaire in seconds with our advance AI technology',
 };
 
 export default function RootLayout({
