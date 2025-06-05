6b2d9242f5cf9bd09f5ecc89dc5516d20c007bf7
theboynextdoor <cman55245@gmail.com>
2025-06-05 16:06:46 +0530

    Completed the hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import BgGradient from '@/components/common/BgGradient';
+import HeroSection from '@/components/home/HeroSection';
+
 export default function HomePage() {
   return (
-    <div className="">
-      <h1>Home...</h1>
+    <div className="relative w-full">
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+      </div>
     </div>
   );
 }
